b6872f6df2dca0778e09f4ffc4b7a00a18b1d1ee
JJH <jjh@JJHui-MacBookPro.local>
2021-07-02 14:28:54 +0900

    [component] edit header responsive / slider responsive

--- src/pages/main.js
+++ src/pages/main.js
@@ -4,12 +4,21 @@ import Footer from '../components/footer/footer';
 import Topbutton from '../components/topbutton/topbutton';
 import ApplyPillButton from '../components/applypillbutton/applypillbutton';
 import './scss/main.scss';
+import Slider from '../components/slider/Slider';
+import landing from '../static/images/slider1.png';
 
 function Main() {
   return (
     <div class="mainWrapper">
       <Header />
-      <div className="sec1" id="top"></div>
+      <div className="sec1" id="top">
+        <div className="sliderWrapper">
+          <Slider />
+        </div>
+        <div className="landingImgWrapper">
+          <img src={landing} alt="" />
+        </div>
+      </div>
       <div className="sec2"></div>
       <div className="sec3"></div>
       <Topbutton />
