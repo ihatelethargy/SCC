62c35008a50ccbedf32e44d6625ad6d52554a030
ahn kyung rok <akrnote97@gmail.com>
2021-07-05 16:56:28 +0900

    [feature] context,exception

--- src/App.js
+++ src/App.js
@@ -6,17 +6,23 @@ import Main from "./pages/main";
 import Detail from "./pages/detail";
 import Form from "./pages/form";
 import NotFound from "./pages/notfound";
+import EduForm from "./pages/edufrom";
+
+import {SccContextProvider} from "./context/scc";
 
 function App() {
   return (
+    <SccContextProvider>
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Main}/>
         <Route path="/detail" component={Detail}/>
         <Route path="/form" component={Form}/>
+        <Route path="/eduform" component={EduForm}/>
         <Route component={NotFound}/>
       </Switch>
     </BrowserRouter>
+    </SccContextProvider>
   );
 }
 
