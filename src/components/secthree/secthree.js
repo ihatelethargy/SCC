8f9b2287ac6ca776bdff2633658600156be9effd
JJH <jjh@JJHui-MacBookPro.local>
2021-07-06 11:38:32 +0900

    [components]섹션  컴포넌트로 분류

---
+++ src/components/secthree/secthree.js
@@ -0,0 +1,13 @@
+import react, { useContext } from 'react';
+import { Route } from 'react-router-dom';
+import './secthree.scss';
+
+function SecThree() {
+  return (
+    <>
+      <div className="sec3">section 3</div>
+    </>
+  );
+}
+
+export default SecThree;
